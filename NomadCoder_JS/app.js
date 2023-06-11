f0b92bb2d204e6b4009b0e25200e047e0340115c
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-06-11 14:17:42 +0900

    #2.8 Functions part Two

--- NomadCoder_JS/app.js
+++ NomadCoder_JS/app.js
@@ -1,9 +1,29 @@
-function sayHello() {
-    console.log("Hello my name is ");
+function sayHello(nameOfPerson, age) {
+    console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
 }
 
-alert();
-console.log();
-sayHello("nico");
-sayHello("dal");
-sayHello("lynn");
+sayHello("nico", 10);
+sayHello("dal", 23);
+sayHello("lynn", 21);
+
+function plus(firstNumber, secondNumber) {
+    console.log(firstNumber + secondNumber);
+}
+
+plus(8, 60);
+
+function divide(a, b) {
+    console.log(a / b);
+}
+
+divide(98, 20);
+
+const player = {
+    name: "nico",
+    sayHello: function(otherPersonsName) {
+        console.log("hello! " + otherPersonsName + " nice to meet you");
+    },
+};
+
+console.log(player.name);
+player.sayHello("lynn");
