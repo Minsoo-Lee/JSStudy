4ec072e9bcceccd0b20e27a05996c7173839de49
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-06-11 13:51:16 +0900

    #2.7 Functions part One

--- NomadCoder_JS/app.js
+++ NomadCoder_JS/app.js
@@ -1,29 +1,9 @@
-const playerName = "nico";
-const playerPoint = 121212;
-const playerHandsome = false;
-const playerFat = "little bit";
-
-// playerArray[0] == name
-// playerArray[1] == points
-// ...
-const playerArray = ["minsoo", 1212, false, "little bit"];
-
-const player = {
-    name: "minsoo",
-    points: 10,
-    handsome: false,
-    fat: "little bit"
+function sayHello() {
+    console.log("Hello my name is ");
 }
 
-console.log(player);
-console.log(player.name);
-console.log(player["name"]);
-
-player.handsome = true;
-console.log(player);
-
-player.lastName = "potato";
-console.log(player);
-
-player.points = player.points + 15;
-console.log(player.points);
+alert();
+console.log();
+sayHello("nico");
+sayHello("dal");
+sayHello("lynn");
