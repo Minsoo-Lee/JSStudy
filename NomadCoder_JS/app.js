68427ab3b4460833af2f5f625b0fa3b32a9a1e1b
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-06-11 14:38:25 +0900

    #2.9 Recap2 - challenge

--- NomadCoder_JS/app.js
+++ NomadCoder_JS/app.js
@@ -1,27 +1,23 @@
-const player = {
-    name: "nico",
-    age: 98,
-}
-
-console.log(player, console);
-
-player.name = "nicolas";
-console.log(player);
-player.sexy = "soon";
-console.log(player);
-
-function plus(potato, salad) {
-    console.log(potato + salad);
-}
-
-plus(5, 10);
-plus(1.33453, 9898);
-plus(9898, 1.33453);
-
 const calculator = {
     add: function(a, b) {
-        console.log(a, b);
+        console.log(a + b);
     },
+    minus: function(a, b) {
+        console.log(a - b);
+    },
+    div: function(a, b) {
+        console.log(a / b);
+    },
+    multi: function(a, b) {
+        console.log(a * b);
+    },
+    powerof: function(a, b) {
+        console.log(a ** b);
+    }
 };
 
-calculator.add(5, 1);
+calculator.add(4, 2);
+calculator.minus(4, 2);
+calculator.div(4, 2);
+calculator.multi(4, 2);
+calculator.powerof(4, 2);
