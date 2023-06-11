9c9ee01e0fe487257eae31f088c796305b6bd413
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-06-11 13:04:05 +0900

    #2.5 Arrays

--- NomadCoder_JS/app.js
+++ NomadCoder_JS/app.js
@@ -1,7 +1,21 @@
-let amIFat = true;
-amIFat = null;
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
 
-let something;
+const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+const daysOfWeekString = ["mon", "tue", "wed", "thu", "fri", "sat"];
 
-console.log(amIFat);
-console.log(something);
+const nonsense = [1, 2, "hello", false, null, true, undefined, "nico"]
+
+console.log(daysOfWeek, nonsense, daysOfWeekString);
+
+// Get Item from Array
+console.log(daysOfWeekString[4]);
+
+// Add one more day to the array
+daysOfWeekString.push("sun");
+console.log(daysOfWeekString);
