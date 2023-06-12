46cb1f666b7d2f06a2f4d3012a7d1cb74c5845d4
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-06-12 22:41:55 +0900

    #3.6 CSS in Javascript

--- NomadCoder_JS/app.js
+++ NomadCoder_JS/app.js
@@ -1,40 +1,15 @@
 const h1 = document.querySelector(".hello:first-child h1");
-console.dir(h1);
 
 function handleTitleClick() {
-    h1.style.color = "blue";
+    const currentColor = h1.style.color;
+    let newColor;
+
+    if (currentColor === "blue") {
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("copier!");
-}
-
-function handleWindowOffline() {
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-    alert("So good");
-}
-
-h1.onclick = handleTitleClick;
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-// .removeEventListner로 전부 삭제 가능 -> EventListner 권장
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener ("click", handleTitleClick);
