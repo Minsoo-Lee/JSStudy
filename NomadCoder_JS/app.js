0d19861bbbff159c25d687f1cdef11a4de52d847
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-06-12 22:07:35 +0900

    #3.4 Events Part Two

--- NomadCoder_JS/app.js
+++ NomadCoder_JS/app.js
@@ -1,20 +1,18 @@
-const title = document.getElementById("title");
-title.innerText = "Got you!";
+const title = document.querySelector(".hello:first-child h1");
+console.dir(title);
 
-console.log(title.id);
-console.log(title.className);
+function handleTitleClick() {
+    title.style.color = "blue";
+}
 
-// const title2 = document.getElementById("hello");
-// title2.innerText = "Got you!";
+function handleMouseEnter() {
+    title.innerText = "Mouse is here!";
+}
 
-const hellos = document.getElementsByClassName("hello");
-console.log(hellos);
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone!";
+}
 
-const title3 = document.getElementsByTagName("h1");
-console.log(title3);
-
-const title4 = document.querySelector(".hello1 h2");
-console.log(title4);
-
-const title5 = document.querySelectorAll(".hello1 h2");
-console.log(title5);
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
